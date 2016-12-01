a718baf585d78e107016c96888cb934b950d4984
Jim Barnes <james.barnes@ucf.edu>
2016-12-01 08:35:33 -0500

    Removed clearRemoteCache call.

--- src/js/ucf-degree-search.js
+++ src/js/ucf-degree-search.js
@@ -12,8 +12,6 @@ var ucfDegreeSearch = function($) {
     datumTokenizer: datumTokenizer
   });
 
-  engine.clearRemoteCache();
-
   $('.degree-search-typeahead').typeahead({
     minLength: 3,
     highlight: true
