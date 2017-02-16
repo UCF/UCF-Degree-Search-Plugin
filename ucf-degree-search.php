43d5321a32f5e1de8dbc6f81b4cc99751be2403e
Jim Barnes <james.barnes@ucf.edu>
2017-02-16 15:45:49 -0500

    Bumped version number.

--- ucf-degree-search.php
+++ ucf-degree-search.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Degree Search Plugin
 Description: Provides a typeahead search interface for UCF Degrees
-Version: 0.2.0
+Version: 0.2.1
 Author: UCF Web Communications
 License: GPL3
 */
