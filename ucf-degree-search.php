6073634eee2280203f6303581166e44a43ec49d7
Cadie Brown <cadie.brown@gmail.com>
2018-10-12 09:38:48 -0400

    Bump version number

--- ucf-degree-search.php
+++ ucf-degree-search.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Degree Search Plugin
 Description: Provides utilities for listing and searching against UCF Degrees.
-Version: 0.7.0
+Version: 0.7.1
 Author: UCF Web Communications
 License: GPL3
 */
