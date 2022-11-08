084c4f255c5ad1dfc7dd2cf0742fa2fbf8dd58e2
RJ Bruneel <rj.bruneel@ucf.edu>
2022-11-08 16:48:05 -0500

    pagination updates

--- src/angular/src/app/search/pagination/pagination.component.ts
+++ src/angular/src/app/search/pagination/pagination.component.ts
@@ -1,36 +1,24 @@
-import { Params } from './../search-results/params';
-import { Subscription } from 'rxjs';
-import { Results } from 'src/app/search/search-results/results';
-import { SearchService } from "./../search-results/search.service";
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Params } from "./../search-results/params";
+import { Results } from "src/app/search/search-results/results";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-pagination",
   templateUrl: "./pagination.component.html",
   styleUrls: ["./pagination.component.scss"],
 })
-export class PaginationComponent implements OnInit, OnDestroy {
+export class PaginationComponent implements OnInit {
+  @Input() results: Results | undefined;
 
   pages!: Array<number>;
   width!: number;
-  results!: Results;
   params!: Params;
-  subscription: Subscription;
 
-  constructor(private searchService: SearchService) {
-    this.subscription = this.searchService.results$.subscribe((results) => {
-      console.log('results: ', results);
-      this.results = results;
-      this.pagination();
-    });
+  constructor() {}
 
-    this.subscription = this.searchService.params$.subscribe((params) => {
-      console.log('params: ', params);
-      this.params = params;
-    });
+  ngOnInit(): void {
+    this.pagination();
   }
-
-  ngOnInit(): void {}
 
   nextPage() {
     throw new Error("Method not implemented.");
@@ -43,31 +31,27 @@ export class PaginationComponent implements OnInit, OnDestroy {
   }
 
   pagination() {
-    console.log('pagination');
-    this.width = 1000; // TODO (container.nativeElement as HTMLElement).offsetWidth;
-    let pagePad = 4; // (container.nativeElement as HTMLElement).offsetWidth < 768 ? 2 : 4;
+    if (this.results) {
+      this.width = 1000; // TODO (container.nativeElement as HTMLElement).offsetWidth;
+      let pagePad = 4; // (container.nativeElement as HTMLElement).offsetWidth < 768 ? 2 : 4;
 
-    let startPage =
-      this.results.currentPage - pagePad < 1
-        ? 1
-        : this.results.currentPage - pagePad;
-    let endPage =
-      this.results.currentPage + pagePad > this.results.totalPages
-        ? this.results.totalPages
-        : this.results.currentPage + pagePad;
+      let startPage =
+        this.results.currentPage - pagePad < 1
+          ? 1
+          : this.results.currentPage - pagePad;
+      let endPage =
+        this.results.currentPage + pagePad > this.results.totalPages
+          ? this.results.totalPages
+          : this.results.currentPage + pagePad;
 
-    // Reset the array
-    this.pages = new Array<number>();
+      // Reset the array
+      this.pages = new Array<number>();
 
-    for (var i = startPage; i <= endPage; i++) {
-      this.pages.push(i);
+      for (var i = startPage; i <= endPage; i++) {
+        this.pages.push(i);
+      }
+
+      console.log("pages: ", this.pages);
     }
-
-    console.log("pages: ", this.pages);
-  }
-
-  ngOnDestroy() {
-    // prevent memory leak when component destroyed
-    this.subscription.unsubscribe();
   }
 }
