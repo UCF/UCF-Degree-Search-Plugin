eee204e3284fe707e77c9917c81ed81ddb6bf36b
Jim Barnes <jimbarnesdeveloper@gmail.com>
2023-04-07 13:44:19 -0400

    Modified angular code to include trailing slashes on degree search views

--- src/angular/src/app/app-routing.module.ts
+++ src/angular/src/app/app-routing.module.ts
@@ -3,7 +3,38 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './search/home/home.component';
 
 const routes: Routes = [
-  {path: '**', component: HomeComponent}
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'college/:selectedCollege/',
+    component: HomeComponent
+  },
+  {
+    path: 'college/:selectedCollege/search/:search/',
+    component: HomeComponent
+  },
+  {
+    path: 'search/:search/',
+    component: HomeComponent
+  },
+  {
+    path: ':selectedProgramType/',
+    component: HomeComponent
+  },
+  {
+    path: ':selectedProgramType/college/:selectedCollege/search/:search/',
+    component: HomeComponent
+  },
+  {
+    path: ':selectedProgramType/college/:selectedCollege/',
+    component: HomeComponent
+  },
+  {
+    path: ':selectedProgramType/search/:search/',
+    component: HomeComponent
+  }
 ];
 
 @NgModule({
