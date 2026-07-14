3813e794059cdd3a3dac3910c0ba0cf5c5883263
Jim Barnes <james.barnes@ucf.edu>
2026-07-14 10:26:54 -0400

    Added an alias to the degree-list shortcode

--- shortcodes/ucf-degree-external-list-shortcode.php
+++ shortcodes/ucf-degree-external-list-shortcode.php
@@ -33,4 +33,11 @@ if (! class_exists('UCF_External_Degree_List_Shortcode')) {
 	if (! shortcode_exists('ucf-external-degree-list')) {
 		add_shortcode('ucf-external-degree-list', array('UCF_External_Degree_List_Shortcode', 'shortcode'));
 	}
+
+	/**
+	 * Create an alias for the ucf-external-degree-list shortcode to degree-list
+	 */
+	if ( ! shortcode_exists( 'degree-list' ) ) {
+		add_shortcode( 'degree-list', array( 'UCF_External_Degree_List_Shortcode', 'shortcode' ) );
+	}
 }
